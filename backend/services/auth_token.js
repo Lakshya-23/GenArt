82f9dda927da97abda733213f00c46f32e189e5a
Lakshya-23 <Lakshya_Varshney@Outlook.com>
2025-06-28 23:43:20 +0530

    first commit

--- backend/services/auth_token.js
+++ backend/services/auth_token.js
@@ -13,6 +13,9 @@ function createToken(id,email,res){
 
     res.cookie('uid',token,{
         maxAge:7*24*60*60*1000,
+        httpOnly:true,          //prevent XSS cross site scripting attacks
+        sameSite:process.env.NODE_ENV === "production" ? "None" : "Lax",      //CSRF attacks cross-site request forgery attack
+        secure: process.env.NODE_ENV === 'production',
     })
 
     return token 
